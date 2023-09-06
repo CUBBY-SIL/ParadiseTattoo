903da989e0763d7ca38de3fd211fe2aca797f9d1
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-09-06 19:48:20 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?
+
+    $arrContent = array(
+        '1'=>''
+        
+    );
+
+    $GLOBALS['arrContent'] = $arrContent;
+    
+    define('GET_ID_SECTION', 'id');
+    define('DIR_INCLUDE_SECTIONS', 'sections/');
+    define('ERR_FILE_NO_EXISTS', 'err404.html');
+    
+?>
---
+++ sections/section0.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows1251">
+<title><?php echo $title; ?></title>
+<link rel="stylesheet" href="style/style1.css">
+
+</head>
+    <body>
+        
+        
+    </body>
+</html>
