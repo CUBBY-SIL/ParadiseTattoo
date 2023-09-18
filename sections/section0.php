9f048ca1f8c2fff91b14095af88fbe1c0f975b91
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-09-19 01:35:30 +0700

    Сверстал главную страницу

--- sections/section0.php
+++ sections/section0.php
@@ -3,11 +3,116 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=windows1251">
 <title><?php echo $title; ?></title>
-<link rel="stylesheet" href="style/style1.css">
 
 </head>
     <body>
+        <h1>Студия художественной татуировки<br>в центре Новосибирска</h1>
         
+        <img src="img/studio.jpg" class="studio-img">
+
+        <p class="content-tittle">Ориентируемся на клиента, его идею и пожелания</p>
+        <p class="content-tittle">портфолио наших<br>
+                            <span class="red-span">тату мастеров</span>
+        </p>
+
+        <p class="content-information">Хотите сделать татуировку в Новосибирске?<br>
+            Посмотрите, что мы умеем - вам обязательно понравится.<br>
+            Тату любого стиля, любой сложности от лучших мастеров Новосибирска!
+        </p>
+
+        <!-- SLIDER -->
+        <div class="swiper">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-1.jpg" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-2.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-3.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-4.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-5.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-6.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-7.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-8.png" class="slider-img">
+              </div>
+              <div class="swiper-slide">
+                <img src="img/slider/slider-img-9.png" class="slider-img">
+              </div>
+            </div><!-- DIV SWIPER-WRAPPER -->
+            <div class="swiper-pagination"></div>
+          
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div><!-- DIV SWIPER -->
+
+        <p class="content-tittle">почему <b><span class="red-span">лучше</span><br>сделать татуировку у нас</p></b>
         
+        <p class="content-information">Ищите лучший тату салон в Новосибирске?<br>
+            В нашем тату салоне только лучшие мастера,<br>
+            высшее качество, 100% безопасность и выгодные цены!
+        </p>
+
+        <section class="container-elements">
+            <div class="elements-item">
+                <div class="elements-item-img"><img src="img/icons-crown.png"></div>
+                <div class="elements-item-information">
+                    <p class="elements-item-information-text">
+                        <b>Высокий уровень<br></b>
+                        исполнения, огромный опыт и <br>профессиональный сервис
+                    </p>
+                </div><!-- DIV ELEMENTS ITEM INFORMATION -->
+            </div><!-- DIV ELEMENTS ITEM -->
+            <div class="elements-item">
+                <div class="elements-item-img"><img src="img/icons-shield.png"></div>
+                <div class="elements-item-information">
+                    <p class="elements-item-information-text">
+                        <b>Высокий уровень<br></b>
+                        исполнения, огромный опыт и <br>профессиональный сервис
+                    </p>
+                </div><!-- DIV ELEMENTS ITEM INFORMATION -->
+            </div><!-- DIV ELEMENTS ITEM -->
+            <div class="elements-item">
+                <div class="elements-item-img"><img src="img/icons-brush.png"></div>
+                <div class="elements-item-information">
+                    <p class="elements-item-information-text">
+                        <b>Профессиональное<br>
+                        оборудование</b> и <br>краска высшего качества от <br>мировых брендов
+                    </p>
+                </div><!-- DIV ELEMENTS ITEM INFORMATION -->
+            </div><!-- DIV ELEMENTS ITEM -->
+            <div class="elements-item">
+                <div class="elements-item-img"><img src="img/icons-macbook.png"></div>
+                <div class="elements-item-information">
+                    <p class="elements-item-information-text">
+                        <b>Приемлемые цены</b><br>
+                        которые не ударят по вашему<br> бюджету, плюс постоянные<br> акции и скидки
+                    </p>
+                </div><!-- DIV ELEMENTS ITEM INFORMATION -->
+            </div><!-- DIV ELEMENTS ITEM -->
+            <div class="elements-item">
+                <div class="elements-item-img"><img src="img/icons-medal.png"></div>
+                <div class="elements-item-information">
+                    <p class="elements-item-information-text">
+                        <b>Профессиональные мастера</b><br>
+                        с художественным образованием<br> <b>знающие</b> свое <b>дело</b>
+                    </p>
+                </div><!-- DIV ELEMENTS ITEM INFORMATION -->
+            </div><!-- DIV ELEMENTS ITEM -->
+        </section><!-- SECTION CONTAINER ELEMENTS -->
+
+        <script src="js/swiper-bundle.min.js"></script>
+        <script src="js/script-slider.js"></script>
     </body>
 </html>
