eaa86aed7fccc8c6e411170cfe6cbdae7b0f5f6f
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-09-20 23:40:32 +0700

    Сверстал секцию "Работы"

---
+++ sections/section2.php
@@ -0,0 +1,147 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows1251">
+<title><?php echo $title; ?></title>
+
+</head>
+    <body>
+        <h2 class="content-tittle">Работы наших тату мастеров</h2>
+
+        <p class="content-information">Знакомьтесь с нашими работами от одних<br>
+                                            из лучших мастеров города
+        </p>
+
+        <div class="container-works">
+            <!-- WORKS Гордеев Даниил -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_gd_1.jpg" class="work-img-pic"></div>
+                <div class="work-description">Гордеев Даниил</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_gd_2.jpg" class="work-img-pic"></div>
+                <div class="work-description">Гордеев Даниил</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_gd_3.jpg" class="work-img-pic"></div>
+                <div class="work-description">Гордеев Даниил</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_gd_4.jpg" class="work-img-pic"></div>
+                <div class="work-description">Гордеев Даниил</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_gd_5.jpg" class="work-img-pic"></div>
+                <div class="work-description">Гордеев Даниил</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_gd_6.jpg" class="work-img-pic"></div>
+                <div class="work-description">Гордеев Даниил</div>
+            </div><!-- DIV WORK-BOX -->
+
+            <!-- WORKS Романова Дарья -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rd_1.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова Дарья</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rd_2.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова Дарья</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rd_3.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова Дарья</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rd_4.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова Дарья</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rd_5.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова Дарья</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rd_6.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова Дарья</div>
+            </div><!-- DIV WORK-BOX -->
+
+            <!-- WORKS Смирнова Анна -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_sa_1.jpg" class="work-img-pic"></div>
+                <div class="work-description">Смирнова Анна</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_sa_2.jpg" class="work-img-pic"></div>
+                <div class="work-description">Смирнова Анна</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_sa_3.jpg" class="work-img-pic"></div>
+                <div class="work-description">Смирнова Анна</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_sa_4.jpg" class="work-img-pic"></div>
+                <div class="work-description">Смирнова Анна</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_sa_5.jpg" class="work-img-pic"></div>
+                <div class="work-description">Смирнова Анна</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_sa_6.jpg" class="work-img-pic"></div>
+                <div class="work-description">Смирнова Анна</div>
+            </div><!-- DIV WORK-BOX -->
+
+            <!-- WORKS Романова София -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rs_1.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова София</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rs_2.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова София</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rs_3.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова София</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rs_4.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова София</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rs_5.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова София</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_rs_6.jpg" class="work-img-pic"></div>
+                <div class="work-description">Романова София</div>
+            </div><!-- DIV WORK-BOX -->
+
+            <!-- WORKS Волков Максим -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_vm_1.jpg" class="work-img-pic"></div>
+                <div class="work-description">Волков Максим</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_vm_2.jpg" class="work-img-pic"></div>
+                <div class="work-description">Волков Максим</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_vm_3.jpg" class="work-img-pic"></div>
+                <div class="work-description">Волков Максим</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_vm_4.jpg" class="work-img-pic"></div>
+                <div class="work-description">Волков Максим</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_vm_5.jpg" class="work-img-pic"></div>
+                <div class="work-description">Волков Максим</div>
+            </div><!-- DIV WORK-BOX -->
+            <div class="work-box">
+                <div class="work-img"><img src="img/worksExample/ex_vm_6.jpg" class="work-img-pic"></div>
+                <div class="work-description">Волков Максим</div>
+            </div><!-- DIV WORK-BOX -->
+        </div><!-- DIV CONTAINER-WORKS -->
+    </body>
+</html>
