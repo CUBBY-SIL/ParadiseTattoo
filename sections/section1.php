3897c15cf292aa6e61df75c4e9a47938545a3389
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-09-20 19:49:50 +0700

    Сверстал секцию "Мастера"

---
+++ sections/section1.php
@@ -0,0 +1,107 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows1251">
+<title><?php echo $title; ?></title>
+
+</head>
+    <body>
+        <p class="content-tittle">мастера студии</p>
+
+        <p class="content-information">Среди наших мастеров, вы обязательно найдете<br>
+            <span class="red-span"><b>СВОЕГО</b></span>
+        </p>
+
+        <div class="container-masters">
+            <div class="master-cell">
+                <div class="cell-master-img"><img src="img/masters/master-maksim.png" class="master-img"></div>
+                <div class="cell-master-info">
+                    <div class="master-tittle"><span class="red-span">Волков Максим</span></div>
+                    <div class="master-info-detail">
+                        <p class="info-detail-text">
+                            Минимальная стоимость работы: 3.000р<br>
+                            Стоимость часа работы: 2.400р<br>
+                            Сеанс (4 часа): 10.000р<br>
+                            Сеанс дневной (не менее 6-ти часов): 15.000р
+                        </p>
+                        <p class="info-detail-text">
+                            Работает в стиле: полинезия, кельтский, хайда, скандинавия, трайбл, графика, геометрия
+                        </p>
+                    </div><!-- DIV MASTER-INFO-DETAIL -->
+                </div><!-- DIV CELL-MASTER-INFO -->
+            </div><!-- DIV MASTER-CELL -->
+
+            <div class="master-cell">
+                <div class="cell-master-info">
+                    <div class="master-tittle"><span class="red-span">Романова Дарья</span></div>
+                    <div class="master-info-detail">
+                        <p class="info-detail-text">
+                            Минимальная стоимость работы: 2.000р<br>
+                            Стоимость часа работы: 1.800р<br>
+                            Сеанс (4 часа): 8.000р<br>
+                            Сеанс дневной (не менее 6-ти часов): 12.000р
+                        </p>
+                        <p class="info-detail-text">
+                            Работает в стиле: чб, реализм, геометрия, мандала, минимализм, скетч-стайл, леттеринг, блэкворк
+                        </p>
+                    </div><!-- DIV MASTER-INFO-DETAIL -->
+                </div><!-- DIV CELL-MASTER-INFO -->
+                <div class="cell-master-img"><img src="img/masters/master-daria.png" class="master-img"></div>
+            </div><!-- DIV MASTER-CELL -->
+
+            <div class="master-cell">
+                <div class="cell-master-img"><img src="img/masters/master-daniil.png" class="master-img"></div>
+                <div class="cell-master-info">
+                    <div class="master-tittle"><span class="red-span">Гордеев Даниил</span></div>
+                    <div class="master-info-detail">
+                        <p class="info-detail-text">
+                            Минимальная стоимость работы: 2.800р<br>
+                            Стоимость часа работы: 2.000р<br>
+                            Сеанс (4 часа): 7.000р<br>
+                            Сеанс дневной (не менее 6-ти часов): 12.000р
+                        </p>
+                        <p class="info-detail-text">
+                            Работает в стиле: геометрия, графика, скетч-стайл
+                        </p>
+                    </div><!-- DIV MASTER-INFO-DETAIL -->
+                </div><!-- DIV CELL-MASTER-INFO -->
+            </div><!-- DIV MASTER-CELL -->
+
+            <div class="master-cell">
+                <div class="cell-master-info">
+                    <div class="master-tittle"><span class="red-span">Смирнова Анна</span></div>
+                    <div class="master-info-detail">
+                        <p class="info-detail-text">
+                            Минимальная стоимость работы: 2.500р<br>
+                            Стоимость часа работы: 1.900р<br>
+                            Сеанс (4 часа): 10.000р<br>
+                            Сеанс дневной (не менее 6-ти часов): 12.000р
+                        </p>
+                        <p class="info-detail-text">
+                            Работает в стиле: универсал
+                        </p>
+                    </div><!-- DIV MASTER-INFO-DETAIL -->
+                </div><!-- DIV CELL-MASTER-INFO -->
+                <div class="cell-master-img"><img src="img/masters/master-anna.png" class="master-img"></div>
+            </div><!-- DIV MASTER-CELL -->
+
+            <div class="master-cell">
+                <div class="cell-master-img"><img src="img/masters/master-sofia.png" class="master-img"></div>
+                <div class="cell-master-info">
+                    <div class="master-tittle"><span class="red-span">Романова София</span></div>
+                    <div class="master-info-detail">
+                        <p class="info-detail-text">
+                            Минимальная стоимость работы: 2.200р<br>
+                            Стоимость часа работы: 2.200р<br>
+                            Сеанс (4 часа): 10.000р<br>
+                            Сеанс дневной (не менее 6-ти часов): 15.000р
+                        </p>
+                        <p class="info-detail-text">
+                            Работает в стиле: треш-полька, реализм, акварель
+                        </p>
+                    </div><!-- DIV MASTER-INFO-DETAIL -->
+                </div><!-- DIV CELL-MASTER-INFO -->
+            </div><!-- DIV MASTER-CELL -->
+        </div><!-- DIV CONTAINER-MASTERS -->
+    </body>
+</html>
