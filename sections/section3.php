cfd95c2174ce6e7172d8c1fec05818aba9c363c6
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-11-24 19:24:01 +0700

    Указал ссылки на секции стилей

--- sections/section3.php
+++ sections/section3.php
@@ -18,49 +18,49 @@
                 <div class="box-style-information">А вы бы хотели сделать свою полинезийскую тату 
                     на островах или в салоне?
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=Polynesia" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
             <div class="box-style sketch">
                 <div class="box-style-tittle">Скетч</div>
                 <div class="box-style-information">Хм... закончена тату или нет? 
                     Тату стиль скетч - мастер загадок
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=Sketch" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
             <div class="box-style celtic">
                 <div class="box-style-tittle">Кельтика</div>
                 <div class="box-style-information">Любите мистику, богатую истори. и хотите, 
                     чтобы тату было со смыслом?
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=Celtic" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
             <div class="box-style realism">
                 <div class="box-style-tittle">Реализм</div>
                 <div class="box-style-information">Самый эффектный и трудоемкий стиль 
                     татуировки "реализм"
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=Realism" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
             <div class="box-style watercolour">
                 <div class="box-style-tittle">Акварель</div>
                 <div class="box-style-information">Один из самых ярких и красивых стилей 
                     татуировки "акварель"
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=Aquarelle" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
             <div class="box-style blackwork">
                 <div class="box-style-tittle">Black Work</div>
                 <div class="box-style-information">Любите минимализм, геометрию и узоры? Тогда 
                     для вас тату в стиле Блэкворк!
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=BlackWork" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
             <div class="box-style minimalism">
                 <div class="box-style-tittle">Минимализм</div>
                 <div class="box-style-information">В эттой статье мы расскажем вам о таком стиле 
                     татуировок как "минимализм"
                 </div>
-                <a href="#" class="link-read-style"><div class="box-style-button">Читать</div></a>
+                <a href="?id=Minimalism" class="link-read-style"><div class="box-style-button">Читать</div></a>
             </div><!-- DIV BOX-STYLE -->
         </div><!-- DIV CONTAINER-STYLES -->
     </body>
