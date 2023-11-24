3f42aa0b31afd4945623eaf937ed60f53eb08f95
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-11-24 18:56:45 +0700

    Добавил секцию стиля "Кельтика"

---
+++ sections/sectionCeltic.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows1251">
+<title><?php echo $title; ?></title>
+
+</head>
+    <body>
+        <h2 class="content-tittle">Стиль татуировки <span class="red-span">Кельтика</span></h2>
+
+        <div class="container-text-title">
+            <p class="content-information">Кельтская татуировка берет свое начало из кельтского искусства, зародившегося много 
+            веков назад. Кто же такие кельты? Это племена, обосновавшиеся на территории Западной и Центральной Европы. 
+            Народ древний, ведь их история исчисляется аж со второго тысячелетия до нашего века.
+            </p>
+
+            <p class="content-information">Началом кельтского искусства принято считать создание в 800-м году нашей эры 
+                рукописи «Книга Колумбы». Писание содержало огромное количество сложных рисунков и узоров, выполненных в 
+                основном в красном, сиреневом, розовом, зеленом и желтом цветах.
+            </p>
+
+            <p class="content-information"><b>История <span class="red-span">кельтских</span> тату</b></p>
+
+            <p class="content-information">В те времена для кельтских воинов тату было оберегом от различных невзгод 
+                (их же много на войне) и магнитом для счастья и благополучия. Исторически сложилось, что основным цветом тату 
+                является черный, дополненный зеленым. Хотя с развитием стиля он становился 
+                более красочным (привет рукописи «Книга Колумбы»).
+            </p>
+
+            <p class="content-information"><b>Значение татуировок</b></p>
+
+            <p class="content-information">Сейчас редко мастер различных стилей объясняет, какое значение имеет будущий 
+                рисунок, а клиенты выбирают тату по внешним признакам, не вдаваясь в смысл. Но такая штука с кельтскими 
+                татуировками не прокатит, они все имеют свое скрытое и глубокое значение.
+            </p>
+
+            <p class="content-information">Кельтский крест. Является самым узнаваемым и популярным рисунком в этом стиле, 
+                имеет много значений, так это вечность, надежда, единство четырех стихий, память.
+            </p>
+            
+            <p class="content-information"><span class="red-span">Магический узел</span> - верность.</p>
+
+            <p class="content-information"><span class="red-span">Бабочки</span> - символ преображения и душ предков.</p>
+
+            <p class="content-information"><span class="red-span">Дракон</span> - могущество и мощь.</p>
+
+            <p class="content-information"><span class="red-span">Бык</span> - невероятная сила.</p>
+            
+            <p class="content-information">Причем все эти рисунки редко существуют сами по себе, они всегда оплетены 
+                сложным узором или даже могут перекликаться друг с другом в одном тату. Получается очень сложно, «узорчато» и красиво.
+            </p>
+
+            <div class="container-styles-examples">
+                <div class="examples-item">
+                    <img class="img-resposive" src="./img/styles/examples/style_kel1.jpg">
+                </div>
+                <div class="examples-item">
+                    <img class="img-resposive" src="./img/styles/examples/style_kel2.jpg">
+                </div>
+                <div class="examples-item">
+                    <img class="img-resposive" src="./img/styles/examples/style_kel3.jpg">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
