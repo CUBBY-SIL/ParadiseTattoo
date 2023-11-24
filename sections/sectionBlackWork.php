68b3a7381fd20a6ea60d743d4088b4de9d9e1b7c
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-11-24 19:16:04 +0700

    Добавил секцию стиля "Black Work"

---
+++ sections/sectionBlackWork.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows1251">
+<title><?php echo $title; ?></title>
+
+</head>
+    <body>
+        <h2 class="content-tittle">Стиль татуировки <span class="red-span">Black Work</span></h2>
+
+        <div class="container-text-title">
+            <p class="content-information">Чтобы понять суть тату в стиле Блэкворк (blackwork), не нужны особые знания 
+                английского языка, переведя это слово, вы догадаетесь, что этот стиль работает с черным цветом. Сейчас 
+                блэкворк невероятно популярен, ведь он содержит в себе элементы минимализма, армия поклонников которого постоянно растет.
+            </p>
+
+            <p class="content-information"><b>Суть стиля стиля <span class="red-span">Black Work</span></b></p>
+
+            <p class="content-information">Основная особенность, это конечно то, что в работе присутствуют большие пятна черного цвета, 
+                которые могут быть более яркими или светлыми за счет используемых полутонов. Но при этом любая тату в  черном цвете 
+                не будет относиться к блэкворку, а вот, например, черный треугольник – это уже классика блэкворка.
+            </p>
+
+            <p class="content-information">Ошибочно мнение, что блэкворк – это закрашенный черным цветом участок тела, 
+                но все не так скучно, рисунки в стиле блэкворка интересны и необычны, что часто достигается за счет точечного нанесения.
+            </p>
+
+            <p class="content-information">Сочетается с другими стилями, например с надписями, дотворком, геометрией и уже 
+                названным выше минимализмом. Также часто блэкворк перекликается с тату в стиле Полинезия из-за использования 
+                узоров на эскизах.
+            </p>
+            
+            <p class="content-information">Часто тату в стиле блэкворк клиенты выбирают, чтобы скрыть различные 
+                неприятные дефекты кожи – шрамы, рубцы. Ведь под черными крупными узорами эти проблемы совсем незаметны. 
+                Помимо этого блэкворк тату служат для перекрытия неудачных работ мастеров с руками не из того места. 
+                Вот только важно понимать, что, если к выбору стиля блэкворк вы подошли неосознанно, то закрасить такой 
+                рисунок в будущем будет почти нереально. Конечно, можно будет попробовать ввести под кожу белую краску, 
+                но при таком решении в основном получается мутное пятно. Вариант с закрашиванием всей тату в черный мы 
+                не рассматриваем, так как это будет жуть.
+            </p> 
+
+            <p class="content-information"><b>Подстили <span class="red-span">Black Work</span></b></p>
+            
+            <p class="content-information">Неотрайбализм</p>
+            <p class="content-information">Арт Брют</p>
+            <p class="content-information">Этнические тату</p>
+            <p class="content-information">Буддистские/тайские тату</p>
+
+            <div class="container-styles-examples">
+                <div class="examples-item">
+                    <img class="img-resposive" src="./img/styles/examples/style_black1.jpg">
+                </div>
+                <div class="examples-item">
+                    <img class="img-resposive" src="./img/styles/examples/style_black2.jpg">
+                </div>
+                <div class="examples-item">
+                    <img class="img-resposive" src="./img/styles/examples/style_black3.jpg">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
