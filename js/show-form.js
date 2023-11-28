932e6b3e0aae39ee902baa8359fe03b95f0417be
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-11-28 14:35:56 +0700

    Добавил модальное окно авторизации/регистрации

---
+++ js/show-form.js
@@ -0,0 +1,67 @@
+window.addEventListener('load', function(){
+    let btns = document.querySelectorAll('.btn-form');
+    let formAut = document.querySelector('.container-login');
+    let formReg = document.querySelector('.container-registration');
+    
+    btns.forEach(item => {
+        item.addEventListener('click', function(e) {
+            showForm(e.target);
+        })
+    })
+
+    let modalButton = document.querySelector('.js-open-modal');
+    let overlay = document.querySelector('#overlay-modal');
+    let closeButtons = document.querySelector('.js-modal-close');
+    let modalElem = document.querySelector('.modal');
+    let body = document.body;
+
+    modalButton.addEventListener('click', function(e){
+        body.classList.add('modal-open');
+
+        modalElem.classList.add('active');
+        overlay.classList.add('active');
+    })
+
+    closeButtons.addEventListener('click', function(){
+
+        body.classList.remove('modal-open');
+        modalElem.classList.remove('active');
+        overlay.classList.remove('active');
+    })
+
+    function showForm(target){
+        let showForm;
+        let hideForm;
+    
+        if(target.classList.contains('btn-into-reg')){
+            showForm = formReg;
+            hideForm = formAut;
+        }
+        else{
+            showForm = formAut;
+            hideForm = formReg;
+        }
+    
+        let animHide = hideForm.animate(
+            [
+                { opacity: 0 },
+            ], { duration: 300 }
+        );
+    
+        let animShow;
+    
+        animHide.addEventListener('finish', function(){
+            hideForm.classList.remove('active');
+    
+            showForm.classList.add('active');
+    
+            animShow = showForm.animate(
+                [
+                    { opacity: 0, },
+                    { opacity: 1, },
+                ], { duration: 300 }
+            );
+        })
+    }
+});
+
